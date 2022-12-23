1265693ce982cb3ccd10ce720c0ad0cbdc7255ae
Artem Morozov <deadtems@yandex.ru>
2022-12-23 18:21:00 +0500

    First commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const wrapper = document.querySelector('.wrapper'),
   inputPart = wrapper.querySelector('.input-part'),
   infoTxt = inputPart.querySelector('.info-txt'),
-  inputField = inputPart.querySelector('input');
+  inputField = inputPart.querySelector('input'),
+  locationBtn = inputPart.querySelector('button');
+
+let api;
 
 const apiKey = 'e9090fe3f93f30462a1ac5b3eb869818';
-console.log(apiKey);
 
 inputField.addEventListener('keyup', (e) => {
   if ((e.key == 'Enter') & (inputField.value != '')) {
@@ -12,7 +14,44 @@ inputField.addEventListener('keyup', (e) => {
   }
 });
 
+locationBtn.addEventListener('click', () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  } else {
+    alert('Your browser not suppor geolocation api');
+  }
+});
+
+function onSuccess(position) {
+  const { latitude, longitude } = position.coords;
+  api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  fetchData();
+}
+
+function onError(error) {
+  infoTxt.innerText = error.message;
+  infoTxt.classList.add('error');
+}
+
 function requestApi(city) {
-  let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-  fetch(api).then((response) => console.log(response.json()));
+  api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  fetchData();
+}
+
+function fetchData() {
+  infoTxt.innerText = 'Getting weather derails...';
+  infoTxt.classList.add('pending');
+  fetch(api)
+    .then((response) => response.json())
+    .then((result) => weatherDetails(result));
+}
+
+function weatherDetails(info) {
+  infoTxt.classList.replace('pending', 'error');
+  if (info.cod === '404') {
+    infoTxt.innerText = `${inputField.value} isn't a valid city name.`;
+  } else {
+    infoTxt.classList.remove('pending', 'error');
+    console.log(info);
+  }
 }
